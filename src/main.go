959a46d2be420a522473ee763bd95f1d692749bd
saschel88 <as@crypta.kz>
2018-06-06 19:05:16 +0600

    New file in src

--- src/main.go
+++ src/main.go
@@ -1,124 +1,59 @@
 package main
 
 import (
-	"encoding/json"
-
-
-	"io/ioutil"
-	"log"
 	"net/http"
-	"strings"
-	"os"
-
+	"log"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/astaxie/beego/logs"
 )
 
-type coinSymbolAndPrice struct {
-	Id        string `json:"id"`
-	Symbol    string `json: "symbol"`
-	Price_usd string `json:"price_usd"`
-	//Price_usd float64 `json:"price_usd,string"` //парсим строковое значение в float64
+
+
+type coinsData struct {
+	//	Price_usd string `json:"price_usd"`
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Symbol string `json:"symbol"`
+
 }
 
-var urlApi = "https://api.coinmarketcap.com/v1/ticker/?limit=1000"
+type jsonStruct struct {
+	Data map[string]coinsData `json:"data"`
 
-//Данная функция применяется, когда нам известен только Symbol валюты
-func dublicateCurrencyList(coinFromUnmarshalingFunc []coinSymbolAndPrice, lenArgs int) (map[string]string, string) { //Функция для переноса распарсенных данных в карту, а так же для  поиска повторений по параметру Symbol
-	strListRepeatCurrency := ""
-	currencyMap := make(map[string]string)
-	if lenArgs > 1 {
-		for i := 0; i < len(coinFromUnmarshalingFunc); i++ { //заполняем карту значениями из массива структур coinFromUnmarshaling
-			_, ok := currencyMap[coinFromUnmarshalingFunc[i].Symbol]
-
-			if ok {
-				strListRepeatCurrency += coinFromUnmarshalingFunc[i].Symbol + ","
-			}
-			currencyMap[coinFromUnmarshalingFunc[i].Symbol] = coinFromUnmarshalingFunc[i].Id + "\t" + coinFromUnmarshalingFunc[i].Price_usd
-		}
-
-	} else {
-		for i := 0; i < len(coinFromUnmarshalingFunc); i++ { //заполняем карту значениями из массива структур coinFromUnmarshaling
-			currencyMap[coinFromUnmarshalingFunc[i].Id] = coinFromUnmarshalingFunc[i].Symbol + "\t" + coinFromUnmarshalingFunc[i].Price_usd
-		}
-	}
-
-	return currencyMap, strListRepeatCurrency
 }
+
+const countCoins  = 50
+var urlApiv2 = "https://api.coinmarketcap.com/v2/ticker/?limit=1"
+var  urlApiv2Listing ="https://api.coinmarketcap.com/v2/listings/"
+
+//Функция получения ID по значению Symbol валюты
+
+//func getIDbySymbol (sliceOfCoinData []coinsData) (map[string]string, string) {
+//
+//}
 
 func main() {
-	lenArgs := len(os.Args)
+
+	coinsData  := jsonStruct{}
 
 
-	coinFromUnmarshaling := []coinSymbolAndPrice{} // объявляем массив структур coinSymbolAndPrice
-
-	resp, err := http.Get(urlApi) //получаем данные с coinmarcetcap c помощью Get запроса по URL = urlApi
+	resp, err := http.Get(urlApiv2) //получаем данные с coinmarcetcap c помощью Get запроса по URL = urlApiv2
 	if err != nil {               //проверка на ошибку
-		log.Fatal("Возникала ошибка: ", err)
+		log.Fatal("Получить данные по GET запросу не удалось: ", err)
 	}
-	defer resp.Body.Close()                  //По окончанию работы закрываем resp.Body
-	respBody, _ := ioutil.ReadAll(resp.Body) // записываем полученную с запроса информацию в []byte  respBody
+	defer resp.Body.Close()
 
-	err = json.Unmarshal(respBody, &coinFromUnmarshaling) // Распарсиваем данные с respBody в массив структур coinFromUnmarshaling
+	respBody, err := ioutil.ReadAll(resp.Body) // записываем полученную с запроса информацию в []byte  respBody
+	if err != nil {               //проверка на ошибку
+		log.Fatal("Считать данные с тела resp в []byte  не удалось: ", err)
+	}
+	logs.Warning(string(respBody))
+	logs.Info(coinsData)
+	err = json.Unmarshal(respBody, &coinsData) // Распарсиваем данные с respBody в массив структур sliceOfCoinData
 	if err != nil {                                       //Проверяем на ошибки
 		log.Fatal("Not UnMarshaling:", err)
 	}
 
-
-	bs, err := ioutil.ReadFile("currencyold.txt") //Считываем с файла перечень валлют, которыми торгуем
-	if err != nil {
-		log.Fatal(err)
-	}
-	currencyListInFile := strings.Split(string(bs), "\n")                   // получаем массив наименований валют из строки string(bs) по раделителю "\n"
-
-	mapCoinSymbolAndPrice, strListRepeatCurrency := dublicateCurrencyList(coinFromUnmarshaling, lenArgs) //вызов функции
-	currencyListInFile = currencyListInFile[:len(currencyListInFile)-1]
-	stringCurrencyAndPrice := "" //пустая строка для формирования списка валют с ценами
-
-	if lenArgs > 1 { //формируем список валюты по ID либо по SYMBOL в зависимости от lenArgs (количество аргументов при вызове программы) если 0 (по умолчанию)то режим работы с ID, else то режим работы с SYMBOL
-		strListRepeatCurrency = strListRepeatCurrency[:len(strListRepeatCurrency)-1]
-		arrayStrListRenameCurrency := strings.Split(strListRepeatCurrency, ",") //получаем массив повторяющихся валют из строки strListRenameCurrency по разделителю ","
-		strListRepeatCurrency=""
-		for k, v := range currencyListInFile {
-
-			for i := 0; i < len(v); i++ {
-
-				if v[i] == 32 || v[i] == 9 {
-					currencyListInFile[k] = v[:i] //формируем массив строк SYMBOL
-					i = 100
-
-				}
-			}
-		}
-
-		for _, v := range currencyListInFile { //проверяем на наличие в нашем списке валют (symbol), повторяющихся из arrayStrListRenameCurrency
-			for j := 0; j < len(arrayStrListRenameCurrency)-1; j++ {
-				if v == arrayStrListRenameCurrency[j] {
-					strListRepeatCurrency += v + ", "
-				}
-			}
-		}
-		strListRepeatCurrency = strListRepeatCurrency[:len(strListRepeatCurrency)-2]
-
-	} else {
-		for k, v := range currencyListInFile {
-
-			for i := 0; i < len(v); i++ {
-
-				if v[i] == 32 || v[i] == 9 {
-					currencyListInFile[k] = v[i+1:] //формируем массив строк ID
-					i = 100
-
-				}
-			}
-		}
-
-	}
-	textMessageAboutRepeatCurrency := "Требуется ручная проверка в связи с тем, что данная валюта имеет повторения \n"
-	for _, v := range currencyListInFile { //формирования списка валют с ценами
-		stringCurrencyAndPrice += v + "\t" + mapCoinSymbolAndPrice[v] + "\n"
-
-	}
-	stringCurrencyAndPriceNew := strings.Replace(stringCurrencyAndPrice, ".", ",", -1) // замена точек на запятые
-	stringCurrencyAndPriceNew += textMessageAboutRepeatCurrency+strListRepeatCurrency
-	ioutil.WriteFile("currencynew.txt", []byte(stringCurrencyAndPriceNew), 0777)
-
+	logs.Info(coinsData)
 }
