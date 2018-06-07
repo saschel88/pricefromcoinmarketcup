b3db36ec562f421afb731c371b91c0021f55f8ea
saschel88 <as@crypta.kz>
2018-06-07 15:01:28 +0600

    Получение ID валюты по Symbol или получение Price по ID

--- src/main.go
+++ src/main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"io/ioutil"
 	"encoding/json"
-	"github.com/astaxie/beego/logs"
+	"strings"
+	"strconv"
+	"os"
 )
 
 // Уровень цены в USD
@@ -29,15 +31,63 @@ type jsonStruct struct {
 	Data map[string]coinsIDNameSymbol `json:"data"`
 
 }
+//Структура для заполнения данных Symbol and Price
+type coinsSymbolPrcie struct {
+	Symbol string
+	Price float64
+}
 
-var urlApiv2 = "https://api.coinmarketcap.com/v2/ticker/?limit=2"
+var urlApiv2 = "https://api.coinmarketcap.com/v2/ticker/?limit=100"
+
+const osargsGetID  = "ID"
 
 
-//Функция получения ID по значению Symbol валюты
+//Функция получения ID по значению Symbol валютыё
+func getIDbySymbol (coinsData jsonStruct) (map[string]int, string, error) {
+	currencyMap := make(map[string]int)
+	strListRepeatCurrency:=""
 
-//func getIDbySymbol (sliceOfCoinData []jsonStruct) (map[string]string, string) {
-//
-//}
+	for _,v :=range coinsData.Data{
+		// Формирование списка валют с одинаковыми Symbol
+		_,ok := currencyMap[v.Symbol]
+		if ok {
+			strListRepeatCurrency += v.Symbol + ","
+		}
+		currencyMap[v.Symbol]=v.ID // Заполнение карты currencyMap:Symbol=ID
+	}
+	if len(strListRepeatCurrency)>1 {
+		strListRepeatCurrency=strListRepeatCurrency[:len(strListRepeatCurrency)-1] //Если есть хоть один элемент в строке, то удалем последний (",")
+	}
+return currencyMap, strListRepeatCurrency, nil // Необходимо отработать условия, при которых возвраащется err
+
+}
+//Функция получения Price по значению ID валютыё
+func getPriceByID (coinsData jsonStruct, currencyListInFile []string) ( string, error) {
+	stringIDSymbolAndPrice:=""
+	currecyIDArray :=make([]int,len(currencyListInFile)) //Создаем слайс int для заполенения его нашими ID
+	for k, v := range currencyListInFile {
+		for i := 0; i < len(v); i++ {
+
+			if v[i] == 32 || v[i] == 9 { // ID является значение от начала до первого пробела, либо TAB
+				currecyIDArray[k],_ = strconv.Atoi(v[:i]) //формируем массив строк ID
+				break
+
+			}
+		}
+	}
+
+// формируем строковый массив с данными по ID, Symbol и Price из coinsData
+	for k, arrayV := range currecyIDArray {
+		for _,v:=range coinsData.Data{
+			if v.ID==arrayV {
+				stringIDSymbolAndPrice+=currencyListInFile[k]+"\t"+strconv.FormatFloat(v.Qoutes.USD.Price, 'f', -1, 64)+"\n" //fmt 'f' означает, что представление формата без экспоненты
+
+			}
+		}
+	}
+return stringIDSymbolAndPrice,nil
+
+}
 
 func main() {
 	coinsData  := jsonStruct{}
@@ -46,17 +96,63 @@ func main() {
 		log.Fatal("Получить данные по GET запросу не удалось: ", err)
 	}
 	defer resp.Body.Close()
-
 	respBody, err := ioutil.ReadAll(resp.Body) // записываем полученную с запроса информацию в []byte  respBody
 	if err != nil {               //проверка на ошибку
 		log.Fatal("Считать данные с тела resp в []byte  не удалось: ", err)
 	}
-
-
 	err = json.Unmarshal(respBody, &coinsData) // Распарсиваем данные с respBody в массив структур sliceOfCoinData
 	if err != nil {                                       //Проверяем на ошибки
 		log.Fatal("Not UnMarshaling:", err)
 	}
 
-	logs.Info(coinsData)
+
+
+	bs, err := ioutil.ReadFile("currencyold.txt") //Считываем с файла перечень валлют, которыми торгуем
+	if err != nil {
+		log.Fatal("НЕ удалось считать данные из файла ",err)
+	}
+
+	currencyListInFile := strings.Split(string(bs), "\n")  // получаем массив наименований валют из строки string(bs) по раделителю "\n"
+
+	// Получени ID валюты по SYMBOL
+	if len(os.Args)>1 && os.Args[1]==osargsGetID {
+
+		currencyMapBySymbol, strListRepeatCurrency, _ := getIDbySymbol(coinsData) //Получение  карты currencyMap:Symbol=ID и списка валют с одинаковыми Symbol
+		stringIDAndSymbol := ""                                                   //пустая строка для формирования списка валют с ID
+		for _, v := range currencyListInFile {
+			_, ok := currencyMapBySymbol[v]
+			if ok {
+				stringIDAndSymbol += strconv.Itoa(currencyMapBySymbol[v]) + "\t" + v + "\n"
+			}
+		}
+		textMessageAboutRepeatCurrency := "\n Требуется ручная проверка в связи с тем, что данная валюта имеет повторения \n"
+		// Осуществляем проверку наличия валют из файла в списке повторяющихся валют
+		if len(strListRepeatCurrency) > 0 {
+			arrayStrListRepeatCurrency := strings.Split(strListRepeatCurrency, ",") //получаем массив повторяющихся валют из строки strListRenameCurrency по разделителю ","
+			strListRepeatCurrency = ""
+			for _, v := range currencyListInFile {
+				for _, val := range arrayStrListRepeatCurrency {
+					if v == val {
+						strListRepeatCurrency += v + ","
+					}
+				}
+			}
+
+			if len(strListRepeatCurrency) > 1 {
+				strListRepeatCurrency = strListRepeatCurrency[:len(strListRepeatCurrency)-1] //Если есть хоть один элемент в строке, то удалем последний (",")
+			}
+			stringIDAndSymbol += textMessageAboutRepeatCurrency + strListRepeatCurrency
+		}
+
+		ioutil.WriteFile("currencynew.txt", []byte(stringIDAndSymbol), 0777)
+
+	}
+
+	//Формирование данных для получения ID Symbol Price
+
+
+	currencyStringToFileNew,_:=getPriceByID(coinsData,currencyListInFile)
+
+
+	ioutil.WriteFile("currencynew.txt", []byte(currencyStringToFileNew), 0777)
 }
