705b58a43d149539e06c670a747bc2c09d5e85ea
saschel88 <as@crypta.kz>
2018-06-09 17:00:44 +0600

    Готово

--- src/main.go
+++ src/main.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"strconv"
 	"os"
-	"github.com/astaxie/beego/logs"
 )
 
 // Уровень цены в USD
@@ -95,23 +94,19 @@ return stringIDSymbolAndPrice,nil
 
 func main() {
 	tmpURLApiV2StartLimit:=""
-	tmpStart:=100
+	tmpStart:=0
 	tmpLimit:="100"
-	for i:=1;i<4 ;i++  {
-		tmpURLApiV2StartLimit=urlApiV2StartLimit+strconv.Itoa(tmpStart*i+1)+"&limit="+tmpLimit
-		logs.Info(tmpURLApiV2StartLimit)
-		tmpURLApiV2StartLimit=""
-
-	}
-
-
-
-
 	coinsData  := jsonStruct{}
-	resp, err := http.Get(urlApiV2) //получаем данные с coinmarcetcap c помощью Get запроса по URL = urlApiv2
+
+	for i:=1;i<4 ;i++  {
+
+		tmpURLApiV2StartLimit=urlApiV2StartLimit+strconv.Itoa(tmpStart+1)+"&limit="+tmpLimit
+
+		resp, err := http.Get(tmpURLApiV2StartLimit) //получаем данные с coinmarcetcap c помощью Get запроса по URL = urlApiv2
 	if err != nil {               //проверка на ошибку
 		log.Fatal("Получить данные по GET запросу не удалось: ", err)
 	}
+		tmpStart+=100
 	defer resp.Body.Close()
 	respBody, err := ioutil.ReadAll(resp.Body) // записываем полученную с запроса информацию в []byte  respBody
 	if err != nil {               //проверка на ошибку
@@ -122,19 +117,24 @@ func main() {
 		log.Fatal("Not UnMarshaling:", err)
 	}
 
-	logs.Warning(len(respBody))
+
+		tmpURLApiV2StartLimit=""
+	}
 
 	bs, err := ioutil.ReadFile("currencyold.txt") //Считываем с файла перечень валлют, которыми торгуем
+
 	if err != nil {
 		log.Fatal("НЕ удалось считать данные из файла ",err)
 	}
+
 
 	currencyListInFile := strings.Split(string(bs), "\n")  // получаем массив наименований валют из строки string(bs) по раделителю "\n"
 
 	// Получени ID валюты по SYMBOL
 	if len(os.Args)>1 && os.Args[1]==osargsGetID {
-
+	//if true{
 		currencyMapBySymbol, strListRepeatCurrency, _ := getIDbySymbol(coinsData) //Получение  карты currencyMap:Symbol=ID и списка валют с одинаковыми Symbol
+
 		stringIDAndSymbol := ""                                                   //пустая строка для формирования списка валют с ID
 		for _, v := range currencyListInFile {
 			_, ok := currencyMapBySymbol[v]
@@ -142,11 +142,16 @@ func main() {
 				stringIDAndSymbol += strconv.Itoa(currencyMapBySymbol[v]) + "\t" + v + "\n"
 			}
 		}
+
+
 		textMessageAboutRepeatCurrency := "\n Требуется ручная проверка в связи с тем, что данная валюта имеет повторения \n"
 		// Осуществляем проверку наличия валют из файла в списке повторяющихся валют
+
 		if len(strListRepeatCurrency) > 0 {
 			arrayStrListRepeatCurrency := strings.Split(strListRepeatCurrency, ",") //получаем массив повторяющихся валют из строки strListRenameCurrency по разделителю ","
+
 			strListRepeatCurrency = ""
+
 			for _, v := range currencyListInFile {
 				for _, val := range arrayStrListRepeatCurrency {
 					if v == val {
@@ -157,8 +162,9 @@ func main() {
 
 			if len(strListRepeatCurrency) > 1 {
 				strListRepeatCurrency = strListRepeatCurrency[:len(strListRepeatCurrency)-1] //Если есть хоть один элемент в строке, то удалем последний (",")
-			}
-			stringIDAndSymbol += textMessageAboutRepeatCurrency + strListRepeatCurrency
+				stringIDAndSymbol += textMessageAboutRepeatCurrency + strListRepeatCurrency
+				}
+
 		}
 
 		ioutil.WriteFile("currencynew.txt", []byte(stringIDAndSymbol), 0777)
